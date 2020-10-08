1f01cca0a63af2906d987ded90b08d518e14ca44
Oh-Su-Jung <tnwjddl1996@gmail.com>
2020-10-08 12:30:26 +0900

    TypeHandler, ResultMap 테스트 완료

---
+++ src/test/java/gradle_mybatis_spring_study/mapper/StudentMapperTest.java
@@ -0,0 +1,54 @@
+package gradle_mybatis_spring_study.mapper;
+
+import static org.junit.Assert.fail;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import gradle_mybatis_spring_study.config.ContextRoot;
+import gradle_mybatis_spring_study.dto.Student;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ContextRoot.class})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class StudentMapperTest {
+	protected static final Log log = LogFactory.getLog(StudentMapperTest.class);
+	
+	@After
+	public void tearDown() throws Exception {
+		System.out.println();
+	}
+	
+	@Autowired
+	private StudentMapper mapper;
+
+	@Test
+	public void testSelectStudentByNo() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Student student = new Student();
+		student.setStudId(1);
+		Student seletedStd = mapper.selectStudentByNo(student);
+		Assert.assertNotNull(seletedStd);
+		log.debug(seletedStd.toString());
+	}
+
+	@Test
+	public void testSelectStudentByNoWithResultMap() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Student student = new Student();
+		student.setStudId(1);
+		Student seletedStd = mapper.selectStudentByNoWithResultMap(student);
+		Assert.assertNotNull(seletedStd);
+		log.debug(seletedStd.toString());
+	}
+
+}
